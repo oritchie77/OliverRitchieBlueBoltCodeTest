1739554af2c3dce976c3d5cda929eec850c86c05
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-04-16 17:27:52 +0100

    added event listeners up to button number 2 in string stuff,

--- moduleTwoJSAndPrimatives/index.js
+++ moduleTwoJSAndPrimatives/index.js
@@ -39,9 +39,77 @@ console.log('JS Loaded and ready to log!')
 console.log({baseData, addOnData})
 
 
-/**
- * Your Event listners and Loggers should be added below
- */
+document.querySelector('#log-baseData').addEventListener('click', () => {
+  console.log(baseData)
+})
+
+document.querySelector('#log-baseData-in-reverse').addEventListener('click', () => {
+  let reverseBaseDate = baseData.reverse()
+  console.log(reverseBaseDate)
+})
+
+document.querySelector('#log-baseData-without-first-item').addEventListener('click', () => {
+  let baseDataWithoutFirstItem = baseData
+  baseDataWithoutFirstItem.shift()
+  console.log(baseDataWithoutFirstItem)
+})
+
+document.querySelector('#log-each-baseData').addEventListener('click', () => {
+  baseData.forEach((item) => {
+    console.log(item)
+  })
+})
+
+document.querySelector('#log-baseDate-addOnData').addEventListener('click', () => {
+  let baseDataWithAddOnData= baseData
+      baseDataWithAddOnData.unshift(addOnData)
+  console.log(baseDataWithAddOnData)
+})
+
+document.querySelector('#log-baseData-update').addEventListener('click', () => {
+  let baseDataWithAddOnDataInIndex2= baseData
+  baseDataWithAddOnDataInIndex2.splice(2, 1, addOnData)
+  console.log(baseDataWithAddOnDataInIndex2)
+})
+
+
+document.querySelector('#log-number-of-fox').addEventListener('click', () => {
+  let string = document.querySelector('#stringText').innerHTML
+  let array = string.split(' ')
+  let numberOfFoxOccurrence = array.filter((word) => {
+    if(word === 'fox') {
+      return true
+    } else {
+      return false
+    }
+  })
+  let numberOfFoxLength = numberOfFoxOccurrence.length
+  console.log(numberOfFoxLength)
+})
+
+document.querySelector('#revers-words').addEventListener('click', () => {
+  let string = document.querySelector('#stringText').innerHTML
+  let paragraphArray = string.split(' ')
+  let reverseParagraphArray = []
+  paragraphArray.forEach((word) => {
+    let wordArray = word.split('')
+    let reversedWordArray = wordArray.reverse()
+    let  reversedWord = reversedWordArray.join('')
+    reverseParagraphArray.push(reversedWord)
+  })
+  let reversParagraphString = reverseParagraphArray.join(' ')
+  document.querySelector("#stringText").innerHTML = reversParagraphString
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
