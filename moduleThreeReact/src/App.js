f4aa6a8779b14d544a001c9a2e03657fabb57257
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-04-17 14:41:48 +0100

    added onclick to show 9 cards

--- moduleThreeReact/src/App.js
+++ moduleThreeReact/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.scss'
 import { useQuery } from '@apollo/react-hooks'
 import GET_PRODUCTS_IN_COLLECTION from './gql/getCollection'
@@ -7,14 +7,22 @@ import productCard from "./components/ProductCard/ProductCard";
 
 
 const App = () =>  {
-  
+
+  const [cardCount, setCardCount] = useState(5)
+
   const {data, loading, error} = useQuery(GET_PRODUCTS_IN_COLLECTION, {
     variables: {
-      count: 5,
+      count: cardCount,
       handle: 'skateboard'
     }
   })
 
+  const showCardsByNumber = (numberOfCards) => {
+    setCardCount(numberOfCards)
+  }
+  const handelClick = () => {
+    showCardsByNumber(9)
+  }
   // This will return a set of ten skateboards this should be renderted using the product card and grid type structure styling as you go.
   console.log(data)
   // explore the data and render your product list accordingly
@@ -25,11 +33,10 @@ const App = () =>  {
     return (<div className="App">Loading....</div>)
   }
 
-
-
   let productCardData = data.collectionByHandle.products.edges
   let product = productCardData.map(product => (product.node.title))
   console.log(product)
+
 
 
   return (
@@ -39,13 +46,11 @@ const App = () =>  {
       <main>
         
         <div className="buttonGroup">
-          <button>Reverse Product Order</button>
+          <button >Reverse Product Order</button>
           <button>Price High to Low</button>
           <button>Price Low to High</button>
-          <button>Display 9 products</button>
+          <button onClick={handelClick}>Display 9 products</button>
         </div>
-
-
 
         <div className="ProductList">
             {productCardData.map(product => (
